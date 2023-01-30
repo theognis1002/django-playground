089c432270bc62b36eed9d647fd8c724850fb11b
theognis1002 <theognis1002@gmail.com>
2023-01-29 22:06:47 -0500

    Update setup.sh and update model charfields

--- apps/products/models.py
+++ apps/products/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 
 class Customer(models.Model):
-    first_name = models.CharField(max_length=50, null=False)
-    last_name = models.CharField(max_length=50, null=False)
-    address = models.CharField(max_length=500, null=False)
-    city = models.CharField(max_length=50, null=False)
-    postcode = models.CharField(max_length=50, null=False)
-    email = models.CharField(max_length=50, null=False, unique=True)
+    first_name = models.CharField(max_length=50, blank=True, null=False)
+    last_name = models.CharField(max_length=50, blank=True, null=False)
+    address = models.CharField(max_length=500, blank=True, null=False)
+    city = models.CharField(max_length=50, blank=True, null=False)
+    postcode = models.CharField(max_length=50, blank=True, null=False)
+    email = models.CharField(max_length=50, blank=True, null=False, unique=True)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
@@ -18,7 +18,7 @@ class Customer(models.Model):
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=50, null=False, unique=True)
+    name = models.CharField(max_length=50, blank=True, null=False, unique=True)
     price = models.IntegerField(null=False)
 
     def __str__(self):
@@ -29,7 +29,7 @@ class Order(models.Model):
     order_date = models.DateField(null=False)
     shipped_date = models.DateField(null=True)
     delivered_date = models.DateField(null=True)
-    coupon_code = models.CharField(max_length=50, null=True)
+    coupon_code = models.CharField(max_length=50, blank=True, null=True)
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE, null=False)
     products = models.ManyToManyField(Product, through="LineItem")
 
--- setup.sh
+++ setup.sh
@@ -2,5 +2,5 @@ python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
 python manage.py migrate
-ython manage.py createsuperuser --no-input --username admin --email admin@gmail.com
+python manage.py createsuperuser --no-input --username admin --email admin@gmail.com
 python manage.py runserver
