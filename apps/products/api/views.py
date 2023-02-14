5e7ce23d4e7c54af2cc2fb7a285777b9ef282042
theognis1002 <theognis1002@gmail.com>
2023-02-13 23:43:40 -0500

    Async viewset test with asyncio

--- apps/products/api/views.py
+++ apps/products/api/views.py
@@ -1,4 +1,8 @@
+import asyncio
+
 from django.contrib.auth import get_user_model
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from .serializers import UserSerializer
@@ -6,6 +10,18 @@ from .serializers import UserSerializer
 User = get_user_model()
 
 
+async def fetch_ip():
+    await asyncio.sleep(3)
+    return "123.42.941"
+
+
 class UserViewSet(ModelViewSet):
     serializer_class = UserSerializer
     queryset = User.objects.all()
+
+    @action(detail=False, methods=["get"])
+    def ip(self, request):
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+        ip = loop.run_until_complete(fetch_ip())
+        return Response({"result": ip})
