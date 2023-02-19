85aa2216bcaa3205bdd454151cbf0f5b9086b7d8
theognis1002 <theognis1002@gmail.com>
2023-02-19 11:45:23 -0500

    Threadpool test in API view

--- apps/products/api/views.py
+++ apps/products/api/views.py
@@ -1,5 +1,7 @@
 import asyncio
+import concurrent.futures
 
+import requests
 from django.contrib.auth import get_user_model
 from rest_framework.decorators import action
 from rest_framework.response import Response
@@ -15,6 +17,12 @@ async def fetch_ip():
     return "123.42.941"
 
 
+def fetch_headers():
+    response = requests.get("http://httpbin.org/headers")
+    print(response.status_code)
+    return response.json()
+
+
 class UserViewSet(ModelViewSet):
     serializer_class = UserSerializer
     queryset = User.objects.all()
@@ -25,3 +33,11 @@ class UserViewSet(ModelViewSet):
         asyncio.set_event_loop(loop)
         ip = loop.run_until_complete(fetch_ip())
         return Response({"result": ip})
+
+    @action(detail=False, methods=["get"])
+    def headers(self, request):
+        with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+            futures = [executor.submit(fetch_headers) for _ in range(2)]
+            for future in concurrent.futures.as_completed(futures):
+                future.result()
+        return Response({"result": "ok"})
